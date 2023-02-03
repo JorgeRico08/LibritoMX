b3cf5c4d3390d7f2f5a9163feec3408ba9f34d67
JorgeRico08 <ricoarvizujorge2002@gmail.com>
2023-02-03 00:28:48 -0600

    Cambios

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,19 @@ var router = express.Router();
 /* Funcionalidades dentro de la app */
 /* GET */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Bienvenido a Libritos MX', pag: 'Home' });
+  res.render('index', { title: 'Book Rental', pag: 'Home' });
 });
 
 router.get('/novedades', function(req, res, next) {
-  res.render('novedades', { title: 'Novedades de Libritos MX', pag: 'Novedades' });
+  res.render('novedades', { title: 'Book Rental', pag: 'Novedades' });
+});
+
+router.get('/delmes', function(req, res, next) {
+  res.render('libroMes', { title: 'Libro del mes', pag: 'Libro del mes' });
+});
+
+router.get('/accesorios', function(req, res, next) {
+  res.render('accesorios', { title: 'Book Rental', pag: 'Accesorios' });
 });
 
 router.get('/error', function(req, res, next) {
@@ -19,7 +27,7 @@ router.get('/error', function(req, res, next) {
 
 /* LOGIN y RUTAS EXTERNAS */
 router.get('/', function(req, res, next) {
-  res.render('login-Register', {title: "Librito MX"});
+  res.render('login-Register', {title: "Book Rental"});
 });
 
 router.get('/recuperarC', function(req, res, next) {
